fbb3b36ebfe4e14d0e6d94604459ece2c5523e8b
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-18 17:14:19 -0300

    add img width

--- pages/index.js
+++ pages/index.js
@@ -13,41 +13,45 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <meta
-          name="description"
-          content="Essa é a descrição teste para o seo do dantas bike"
-        />
+        <title>{title}</title>
+        <meta name="description" content={metaDescription} />
         <meta property="og:url" content="https://seo-final.vercel.app/" />
         <meta property="og:site_name" content="Dantas Bike" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Home" />
-        <meta
-          property="og:description"
-          content="Essa é a descrição teste para o seo do dantas bike"
-        />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={metaDescription} />
         <meta
           property="og:image"
           content="https://seo-final.vercel.app/bikeOGGI.png"
         />
-        <meta property="og:image:secure_url" content="https://seo-final.vercel.app/bikeOGGI.png" />
-     
-        <meta property="og:image:secure_url" content="https://seo-final.vercel.app/bikeOGGI_2.jpg" />
-     
-        
+        <meta
+          property="og:image"
+          content="https://seo-final.vercel.app/bikeOGGI_2.jpg"
+        />
+        <meta property="og:image:width" content="1208" />
+        <meta property="og:image:height" content="638" />
+        <meta
+          property="og:image:secure_url"
+          content="https://seo-final.vercel.app/bikeOGGI.png"
+        />
+        <meta property="og:locale" content="pt-br" />
+
+        <meta
+          property="og:image:secure_url"
+          content="https://seo-final.vercel.app/bikeOGGI_2.jpg"
+        />
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="seo-final.vercel.app" />
         <meta property="twitter:url" content="https://seo-final.vercel.app/" />
-        <meta name="twitter:title" content="Home" />
-        <meta
-          name="twitter:description"
-          content="Essa é a descrição teste para o seo do dantas bike"
-        />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={metaDescription} />
         <meta
           name="twitter:image"
           content="https://seo-final.vercel.app/bikeOGGI.png"
         />
-             <meta
+        <meta
           name="twitter:image"
           content="https://seo-final.vercel.app/bikeOGGI_2.jpg"
         />
