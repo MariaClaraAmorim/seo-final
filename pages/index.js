2c3b87f09c6666806585d8f4c2f081e015ea6a4f
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-18 16:11:22 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+
+import { useRouter } from "next/router";
+
+export default function Home(props) {
+  const router = useRouter();
+
+  const {
+    title = "Home",
+    metaDescription = "Essa é a descrição teste para o seo do dantas bike",
+  } = props;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={metaDescription} />
+        <meta
+          property="og:url"
+          content="https://seo-project-omega.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:image" content="/bikeOGGI.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="seo-project-omega.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://seo-project-omega.vercel.app/"
+        />
+        <meta property="og:image" content="/bikeOGGI.png" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={metaDescription} />
+        <meta name="twitter:image" content="/bikeOGGI.png" />
+      </Head>
+      <div>
+        <h1 className="title">Dantas Bikes SEO final</h1>
+      </div>
+    </>
+  );
+}
