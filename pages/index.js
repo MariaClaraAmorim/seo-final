904e65c9f36be03b392b896951e60b80629e4ad3
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-11-21 11:47:03 -0300

    testa seo para outras paginas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { useRouter } from "next/router";
 
@@ -52,16 +53,16 @@ export default function Home(props) {
 
       <div id="wrap">
         <div id="columns" class="columns_1">
-          <a href="/preview">
+          <Link href="/preview">
             <figure>
               <img src="https://d3ugyf2ht6aenh.cloudfront.net/stores/002/139/087/products/sem-nome-1580450px-10241024px-10241024px-11-88ce7f24ce013b280416639560914528-480-0.png" />
               <figcaption>Bicicleta Caloi Velox 29</figcaption>
               <span class="price">$ 1.135 23</span>
-              <a class="button" href="#">
+              <Link class="button" href="#">
                 Comprar
-              </a>
+              </Link>
             </figure>
-          </a>
+          </Link>
         </div>
       </div>
     </>
